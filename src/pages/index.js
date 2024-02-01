7d078fe9d44f3436fa60367ebdcb0248d2dd33d2
smartdriver321 <tinggaldidisneyland@gmail.com>
2024-02-01 20:35:17 +0700

    Add google authentication for admin

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,22 @@
+import { useSession, signIn, signOut } from 'next-auth/react'
+
 export default function Home() {
-  return <div className={' bg-blue-900 w-screen h-screen'}>tes</div>
+  const { data: session } = useSession()
+
+  if (!session) {
+    return (
+      <div className='bg-blue-900 w-screen h-screen flex items-center'>
+        <div className='text-center w-full'>
+          <button
+            className='bg-white p-2 px-4 rounded-lg'
+            onClick={() => signIn('google')}
+          >
+            Login with Google
+          </button>
+        </div>
+      </div>
+    )
+  }
+
+  return <div>Logged in as {session.user.email}</div>
 }
