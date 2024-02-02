3dc69c3da40e837c03e00311142862ae5d1a2ec1
smartdriver321 <tinggaldidisneyland@gmail.com>
2024-02-02 13:31:24 +0700

    Create aside layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,6 @@
-import { useSession, signIn, signOut } from 'next-auth/react'
+import Layout from '@/components/Layout'
+import React from 'react'
 
 export default function Home() {
-  const { data: session } = useSession()
-
-  if (!session) {
-    return (
-      <div className='bg-blue-900 w-screen h-screen flex items-center'>
-        <div className='text-center w-full'>
-          <button
-            className='bg-white p-2 px-4 rounded-lg'
-            onClick={() => signIn('google')}
-          >
-            Login with Google
-          </button>
-        </div>
-      </div>
-    )
-  }
-
-  return <div>Logged in as {session.user.email}</div>
+  return <Layout>test</Layout>
 }
